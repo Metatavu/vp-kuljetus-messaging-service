0aa13691ac7c7f71a5a8de36a2b6e5be5877ab54
Katja Danilova <katja.danilova@protonmail.com>
2024-08-14 00:45:57 +0300

    manually disable test endpoint

--- src/main/kotlin/fi/metatavu/vp/messaging/TestEndpoint.kt
+++ src/main/kotlin/fi/metatavu/vp/messaging/TestEndpoint.kt
@@ -7,6 +7,9 @@ import jakarta.inject.Inject
 import jakarta.ws.rs.Consumes
 import jakarta.ws.rs.POST
 import jakarta.ws.rs.Path
+import org.eclipse.microprofile.config.inject.ConfigProperty
+import java.util.*
+import kotlin.jvm.optionals.getOrNull
 
 /**
  * Test endpoint to trigger an event, should be accessible only for tests
@@ -19,9 +22,15 @@ class TestEndpoint {
     @Inject
     lateinit var globalEventController: GlobalEventController
 
+    @ConfigProperty(name = "env")
+    lateinit var env: Optional<String>
+
     @POST
     @Consumes("application/json")
     suspend fun triggerEvent(event: GlobalEvent): jakarta.ws.rs.core.Response {
+        if (env.isEmpty || env.getOrNull() != "TEST") {
+            return jakarta.ws.rs.core.Response.status(jakarta.ws.rs.core.Response.Status.FORBIDDEN).build()
+        }
         globalEventController.publish(event)
         return jakarta.ws.rs.core.Response.ok().build()
     }
