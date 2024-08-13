ffb07b0114c2ecac21a65388c1fd275c6c9cdda0
Katja Danilova <katja.danilova@protonmail.com>
2024-08-13 18:11:48 +0300

    reflection

--- src/main/kotlin/fi/metatavu/vp/messaging/events/TelematicsDataGlobalEvent.kt
+++ src/main/kotlin/fi/metatavu/vp/messaging/events/TelematicsDataGlobalEvent.kt
@@ -1,10 +1,12 @@
 package fi.metatavu.vp.messaging.events
 
 import fi.metatavu.vp.messaging.GlobalEventType
+import io.quarkus.runtime.annotations.RegisterForReflection
 import java.util.*
 
 /**
  * Abstract base class for all global events related to telematics data
  */
+@RegisterForReflection
 abstract class TelematicsDataGlobalEvent(type: GlobalEventType, driverId: UUID) : GlobalEvent(type = type) {
 }
