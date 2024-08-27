3e774fec9e6c1d3f9f4e952661c310163e91beb2
Ville Juutila <ville.juutila@gmail.com>
2024-08-27 10:45:57 +0300

    Removed WorkTypeEntity.kt and replaced it with WorkEventType from spec

--- src/main/kotlin/fi/metatavu/vp/messaging/events/DriverWorkEventGlobalEvent.kt
+++ src/main/kotlin/fi/metatavu/vp/messaging/events/DriverWorkEventGlobalEvent.kt
@@ -1,15 +1,15 @@
 package fi.metatavu.vp.messaging.events
 
+import fi.metatavu.vp.api.model.WorkEventType
 import fi.metatavu.vp.messaging.events.abstracts.TelematicsDataGlobalEvent
 import io.quarkus.runtime.annotations.RegisterForReflection
 import java.time.OffsetDateTime
 import java.util.*
 
 @RegisterForReflection
-data class DriverWorkingStateChangeGlobalEvent(
+data class DriverWorkEventGlobalEvent(
     override val driverId: UUID,
-    val workTypeId: UUID,
-    val workingStateNew: WorkingState,
+    val workEventType: WorkEventType,
     val time: OffsetDateTime
 ): TelematicsDataGlobalEvent(GlobalEventType.DRIVER_WORKING_STATE_CHANGE, driverId)
 
--- src/main/kotlin/fi/metatavu/vp/messaging/events/WorkingState.kt
+++
@@ -1,9 +0,0 @@
-package fi.metatavu.vp.messaging.events
-
-import io.quarkus.runtime.annotations.RegisterForReflection
-
-@RegisterForReflection
-enum class WorkingState {
-    WORKING,
-    NOT_WORKING
-}
